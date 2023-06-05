68a54c812040cf49d27f7d65e1e8c86df2074f2b
Vincent Vullo <vincevullo@gmail.com>
2023-06-05 14:45:46 -0400

    fixed actorschema

---
+++ models/actorModel.js
@@ -0,0 +1,15 @@
+const mongoose = require(`mongoose`)
+const Schema = mongoose.Schema
+const db = require(`../db`)
+
+const Actor = new Schema(
+    {
+        name: {type:String, required: true},
+        age: {type: Number, required: true},
+        isAlive: {type: Booleam, required: true},
+    },
+    {timestamps: true}
+)
+
+
+module.exports = actorSchema
