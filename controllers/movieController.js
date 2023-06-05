47602ad92212768c8080547dc042f5a95f208d95
Kevin Li <kevinli617@gmail.com>
2023-06-05 19:02:00 -0400

    updated controllers to populate

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -6,16 +6,16 @@ const getAllMovies = async (req, res) => {
         if(req.query.sort) {
             // console.log(req.query.sort)
             if (req.query.sort == 'asc'){
-                movies = await Movie.find().populate({path: 'reviews', model: 'Review'}).sort({title: 1})
+                movies = await Movie.find().populate({path: 'reviews', model: 'Review'}).populate({path: 'actors', model: 'Actor'}).sort({title: 1})
             } else if (req.query.sort == 'desc') {
-                movies = await Movie.find().populate({path: 'reviews', model: 'Review'}).sort({title: -1})
+                movies = await Movie.find().populate({path: 'reviews', model: 'Review'}).populate({path: 'actors', model: 'Actor'}).sort({title: -1})
             } else if (req.query.sort == 'new') {
-                movies = await Movie.find().populate({path: 'reviews', model: 'Review'}).sort({year_released: -1})
+                movies = await Movie.find().populate({path: 'reviews', model: 'Review'}).populate({path: 'actors', model: 'Actor'}).sort({year_released: -1})
             } else if (req.query.sort == 'old') {
-                movies = await Movie.find().populate({path: 'reviews', model: 'Review'}).sort({title: 1})
+                movies = await Movie.find().populate({path: 'reviews', model: 'Review'}).populate({path: 'actors', model: 'Actor'}).sort({title: 1})
             }
         } else {
-            movies = await Movie.find().populate({path: 'reviews', model: 'Review'})
+            movies = await Movie.find().populate({path: 'reviews', model: 'Review'}).populate({path: 'actors', model: 'Actor'})
         }
         res.status(200).json(movies)
     } catch (e) {
@@ -26,7 +26,7 @@ const getAllMovies = async (req, res) => {
 const getMovieByTitle = async (req, res) => {
     try {
         let searchKey = new RegExp(req.params.title, 'i')
-        const movie = await Movie.find({title: searchKey})
+        const movie = await Movie.find({title: searchKey}).populate({path: 'actors', model: 'Actor'}).populate({path: 'reviews', model: 'Review'})
         if (!movie) throw Error('movie not found')
         res.status(200).json(movie)
     } catch (e) {
